43082908e7a7356350b2d2885342cc856e4c7264
Subhash Nalluru <nsn116@imperial.ac.uk>
2019-01-23 19:18:07 +0000

    [nsn116][be716]: Finish splitting

--- decisiontrees/utils/find_split.py
+++ decisiontrees/utils/find_split.py
@@ -1,10 +1,10 @@
 import math
-import numpy
 
 NUM_ROOMS = 4
 
 
-def calculate_dataset_entropy(labels):
+def calculate_dataset_entropy(dataset):
+    labels = [row[-1] for row in dataset]
     frequencies = [0] * NUM_ROOMS
     for label in labels:
         frequencies[label - 1] += 1
@@ -15,26 +15,37 @@ def calculate_dataset_entropy(labels):
     return entropy
 
 
+def get_dataset_entropy(subset, dataset_size):
+    return len(subset) / dataset_size * calculate_dataset_entropy(subset)
+
+
 def find_best_split(dataset, column_index):
-    split_value = None
+    best_split_value = None
     min_remainder = math.inf
-    feature_label = dataset[:, [column_index, -1]]
-    sorted_feature_label = numpy.sort(feature_label, axis=0)
-    # TODO: Find splits, calculate remainder, find minimum remainder
-    return split_value, min_remainder
+    partition_g = [[row[column_index], row[-1]] for row in dataset]
+    partition_g.sort(key=lambda x: x[0])
+    partition_le = []
+    dataset_size = float(len(partition_g))
+    while len(partition_g) > 1:
+        partition_le.append(partition_g.pop(0))
+        if partition_le[-1][1] != partition_g[0][1]:
+            remainder = get_dataset_entropy(partition_le, dataset_size) + \
+                        get_dataset_entropy(partition_g, dataset_size)
+            if remainder < min_remainder:
+                best_split_value = (partition_le[-1][0] + partition_g[0][0]) / 2
+    return best_split_value, min_remainder
 
 
 def find_split(dataset):
     """find_split
 
-    :param data: matrix of input data, where each column except the last
+    :param dataset: matrix of input data, where each column except the last
     represents a feature and the last column represents the labels. """
 
     split_column_index = 0
     best_split_value = 0
     max_info_gain = -1
-    label_column = [row[-1] for row in dataset]
-    h_dataset = calculate_dataset_entropy(label_column)
+    h_dataset = calculate_dataset_entropy(dataset)
 
     for i in range(dataset.shape[1] - 1):
         split_value, remainder = find_best_split(dataset, i)
